cebedd7ae11799102fa4bd8d71aa90a107c6705c
antifragileathlete <adrianschultze@gmail.com>
2021-01-06 14:23:09 +0100

    added excepotion for string input

--- collatz.py
+++ collatz.py
@@ -1,21 +1,23 @@
 import sys
 
-print("Give number")
-userInput = int(input())
+print("Give number: ")
+try:
+    userInput = int(input())
+    def collatz(number):
+        if number % 2 == 0:
+            global userInput
+            userInput = number // 2
+            print(userInput)
+        else:
+            userInput = 3 * number + 1
+            print(userInput)
+        return userInput
 
+    while collatz(userInput) != 1:
+        try:
+            collatz(userInput)
+        except KeyboardInterrupt:
+            sys.exit()
+except ValueError:
+    print("You have to enter an integer")
 
-def collatz(number):
-    global userInput
-    if number % 2 == 0:
-        userInput = number // 2
-        print(userInput)
-    else:
-        userInput = 3 * number + 1
-        print(userInput)
-    return userInput
-
-while collatz(userInput) != 1:
-    try:
-        collatz(userInput)
-    except KeyboardInterrupt:
-        sys.exit()
